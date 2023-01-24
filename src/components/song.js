5fb213e2d2cefe105a3fa0f768635457a0a32a23
Kalin Ivanov <kalin2578@gmail.com>
2023-01-24 15:25:27 +0200

    update

--- src/components/song.js
+++ src/components/song.js
@@ -1,10 +1,9 @@
 import React from "react";
-// import App from "../App";
 
 const Song = ({ currentSong }) => {
   return (
     <div className="song-container">
-      <img src={currentSong.cover}></img>
+      <img alt={currentSong.name} src={currentSong.cover}></img>
       <h2>{currentSong.name}</h2>
       <h3>{currentSong.artist}</h3>
     </div>
