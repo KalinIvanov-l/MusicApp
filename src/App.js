fe61f52dc690b4b7167824b5343b9ab2e5511dda
Kalin Ivanov <kalin2578@gmail.com>
2023-01-24 12:20:18 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Song from "./components/song";
+import Player from "./components/player";
+import "./styles/app.scss";
 
 function App() {
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
