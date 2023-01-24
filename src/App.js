41646b9ffe6f784106c9484360eea6228b2bf69f
Kalin Ivanov <kalin2578@gmail.com>
2023-01-24 13:50:11 +0200

    update

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import data from "./util";
 
 function App() {
   const [songs, setSongs] = useState(data());
-  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [currentSong, setCurrentSong] = useState(songs[2]);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
