9f9e0197021113fa803c793b770882bba014ee04
Kalin Ivanov <kalin2578@gmail.com>
2023-01-24 21:56:10 +0200

    update

--- src/util.js
+++ src/util.js
@@ -32,6 +32,76 @@ function chillHop() {
       color: ["#EEF0D6", "#847241"],
       audio: "https://mp3.chillhop.com/serve.php/?mp3=49429",
     },
+    {
+      name: "Come In And Stay",
+      artist: "Dotlights",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/09/1a58532ef45339e1bd1a3324635e3cf133f9940b-300x300.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#C0BCDC", "#273373"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=45146",
+    },
+    {
+      name: "Green Valley Trail",
+      artist: "Ruck P",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/09/b7f06cec8a0bef89429dd85f68bb718a31289ec6-300x300.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#232D26", "#976D3E"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=41930",
+    },
+    {
+      name: "Sleep Walker",
+      artist: "J.Folk",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/08/113a9b73235cab31646ef0059864aec70e793dd2-300x300.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#141411", "#6C6056"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=43902",
+    },
+    {
+      name: "Everyday",
+      artist: "Aves",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/07/973ae6f7b847fbfe9e2f6a2efbf2a9121ac8285f-300x300.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#2B3121", "#604F34"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=41890",
+    },
+    {
+      name: "In Minutes",
+      artist: "Shrimpnose",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/07/49bc1653b33bd42e570ecdeba7be86e8bd1b5c38-300x300.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#3F4667", "#F1F8DF"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=41689",
+    },
+    {
+      name: "Hyperspace",
+      artist: "Shrimpnose, nobuddy",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/07/49bc1653b33bd42e570ecdeba7be86e8bd1b5c38-300x300.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#3F4667", "#F1F8DF"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=41702",
+    },
+    {
+      name: "The Flash before my Eyes",
+      artist: "Shrimpnose",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/07/49bc1653b33bd42e570ecdeba7be86e8bd1b5c38-300x300.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#3F4667", "#F1F8DF"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=41700",
+    },
   ];
 }
 
